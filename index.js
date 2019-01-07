8a58d2c338b6e343b9c878ab5d6057c434006f3a
bawas07 <bayu.waskita.07@gmail.com>
2019-01-07 15:53:54 +0700

    create function validate and check

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 class SchemaValidator {
 
-    constructor (schema) {
-        this.schema = schema
+    constructor() {
+
     }
 
+    set schema(schm) {
+        this.schm = schm
+    }
+
+    validate(value) {
+        console.log(this.schm)
+        const keys = Object.keys(value)
+        const err = []
+        const res = {}
+        for (let i = 0; i < keys.length; i++) {
+            const name = keys[i]
+            const result = this.check(name, value[name], this.schm[name].type)
+            if (result !== 'pass') {
+                err.push(result)
+            } else {
+                res[name] = value[name]
+            }
+        }
+        if (err.length > 0) return err
+        return res
+    }
+
+    check(key, value, schema) {
+        console.log({key, value, schema})
+        const type = typeof value
+        console.log(type)
+        console.log(schema)
+        if (schema !== type) {
+            return `parameter "${key}" harus berupa "${schema}"`
+        }
+        return 'pass'
+    }
 }
+
+const x = new SchemaValidator
+x.schema = {
+    name: {
+        type: 'string',
+        required: true
+    },
+    age: {
+        type: 'number'
+    }
+}
+let y = x.validate({name: 'udin', age: 20})
+console.log(y)
