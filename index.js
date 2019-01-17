b772cf73903123467b4940631a615976bbcda5ff
bawas07 <bayu.waskita.07@gmail.com>
2019-01-17 16:42:06 +0700

    edit husky

--- index.js
+++ index.js
@@ -100,38 +100,6 @@ class SchemaValidator {
 
     }
 
-    // /**
-    //  * check parameter's type and return pass or error
-    //  * @param {string} key name of field
-    //  * @param {string} value parameter's data
-    //  * @param {string} lang define return's language
-    //  */
-    // type(key, value, schema) {
-    //     const type = typeof value
-    //     if (schema !== type) {
-    //         return response.type(key, schema, this.lang)
-    //     }
-    //     return 'pass'
-    // }
-
-    // /**
-    //  * check parameter's length and return pass or error
-    //  * @param {string} key name of field
-    //  * @param {string} value parameter's data
-    //  * @param {string} type schema's parameter's type
-    //  * @param {number} length schema's data
-    //  */
-    // min(key, value, type, length) {
-    //     const leng = value.length
-    //     if (leng < length && type === 'string') {
-    //         return response.min(key, length, this.lang)
-    //     }
-    //
-    //     if (type === 'number' && value < length) {
-    //         return response.minNum(key, length, this.lang)
-    //     }
-    //     return 'pass'
-    // }
 }
 
 const x = new SchemaValidator;
@@ -153,6 +121,8 @@ const input = {
     name: 'usep',
     age: 5
 };
+
+console.log('trying to make a schema validator');
 
 const run = async () => {
     console.log('\n\n----------------');
